f31fadac571e7d024815c02862d42d6f1da89826
nugliar <hashan.hall@gmail.com>
2021-08-23 19:14:54 -0500

    update

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const dns = require('dns');
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
@@ -28,35 +29,58 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', function(req, res) {
-  let urlResult;
-
-  urlModel.findOne({
-    url: req.body.url
-  }, function(err, result) {
-    if (err) console.error(err);
-    urlResult = result.results;
+app.get('/api/clear', function(req, res) {
+  UrlModel.remove({}, function(err, result) {
+    if (err) return next(err);
+    res.send('done')
   })
+})
 
-  if (urlResult !== null) {
+app.post('/api/shorturl', function(req, res, next) {
+  const urlData = new URL(req.body.url)
+  const hostName = urlData.hostname || ''
+  const pathName = urlData.pathname || ''
+  const href = [hostName, ...pathName.split('/').filter(i => i)].join('/')
 
-    res.json({
-      original_url: urlData.url,
-      short_url: urlData._id
+  dns.lookup(hostName, function(err) {
+    if (err) return next(new Error('Hostname not found: ' + hostName));
+
+    UrlModel.findOne({
+      url: href
+    }, function(err, result) {
+
+      if (err) return next(err);
+
+      if (result) {
+        res.json({
+          original_url: result.url,
+          short_url: result._id
+        })
+
+      } else {
+        const urlDoc = new UrlModel()
+        urlDoc.url = href
+
+        urlDoc.save(function(err, result) {
+
+          if (err) return next(err);
+
+          res.json({
+            original_url: result.url,
+            short_url: result._id
+          })
+        })
+      }
     })
+  })
+})
 
-  } else {
-
-    const urlDoc = new urlModel()
-    urlDoc.url = urlData.url
-
-    urlDoc.save(function(err, result) {
-      if (err) console.error(err);
-      res.json({
-        original_url: result.url,
-        short_url: result._id
-      })
-    })
+app.use(function(err, req, res, next) {
+  if (err) {
+    res
+      .status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'Server Error')
   }
 })
 
